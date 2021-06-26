12a4f25b3305e0a96dfaa526806b008ccc1ab4a4
borislavstoychev <stoy4ew@gmail.com>
2021-06-26 14:36:00 +0300

    test

--- expenses_tracker2/expenses_tracker2/expenses/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from expenses_tracker2.expenses import models
-
-admin.site.register(models.Expense)
--- expenses_tracker2/expenses_tracker2/profiles/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from expenses_tracker2.profiles import models
-
-admin.site.register(models.Profile)
