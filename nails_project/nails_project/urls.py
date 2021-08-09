697705dd99cb8386ba2b42cf53f0bf730337e21e
borislavstoychev <stoy4ew@gmail.com>
2021-08-09 14:58:27 +0300

    added bild in password reset

--- nails_project/nails_project/urls.py
+++ nails_project/nails_project/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('nails/', include('nails_project.sonq_nails.urls')),
-    path('', include('nails_project.common.urls')),
-    path('account/', include('nails_project.accounts.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('nails/', include('nails_project.sonq_nails.urls')),
+                  path('', include('nails_project.common.urls')),
+                  path('account/', include('nails_project.accounts.urls')),
+                  path('accounts/', include('django.contrib.auth.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
