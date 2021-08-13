1cfc594d5369a7cb94b6c0d8a4c5ab446492a8ae
borislavstoychev <stoy4ew@gmail.com>
2021-08-13 11:55:14 +0300

    added singnal for pre-delete nails object to remove cloudinary image

--- nails_project/nails_project/sonq_nails/signals.py
+++ nails_project/nails_project/sonq_nails/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, pre_delete
 from cloudinary import uploader
 from nails_project.sonq_nails.models import Nails
 
@@ -17,3 +17,14 @@ def remove_old_image(sender, instance, **kwargs):
             except AttributeError:
                 uploader.destroy(old_image.public_id)
 
+
+@receiver(pre_delete, sender=Nails)
+def delete_media_when_account_deleted(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_image = Nails.objects.get(pk=instance.pk).image
+        except Nails.DoesNotExist:
+            return
+        else:
+            if old_image.public_id:
+                uploader.destroy(old_image.public_id)
